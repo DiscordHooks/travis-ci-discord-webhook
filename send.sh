941afa6f61631f9de66593c95ab8e50b038db3aa
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2017-12-10 20:25:12 +0530

    Fixed a typo

--- send.sh
+++ send.sh
@@ -70,4 +70,4 @@ WEBHOOK_DATA='{
 }'
 
 (curl -v --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
-  && echo -e "\\n[Webhook]: Successfully set the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
