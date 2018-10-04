c30ebbbc5d87b94b1f5aa7a172188b5b6a4bc220
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-10-04 04:30:28 +0000

    feat: add support for travis-ci.com
    Use the `TRAVIS_BUILD_WEB_URL` environment variable to
    link to the build log.

--- send.sh
+++ send.sh
@@ -51,7 +51,7 @@ WEBHOOK_DATA='{
     "color": '$EMBED_COLOR',
     "author": {
       "name": "Job #'"$TRAVIS_JOB_NUMBER"' (Build #'"$TRAVIS_BUILD_NUMBER"') '"$STATUS_MESSAGE"' - '"$TRAVIS_REPO_SLUG"'",
-      "url": "https://travis-ci.org/'"$TRAVIS_REPO_SLUG"'/builds/'"$TRAVIS_BUILD_ID"'",
+      "url": "'"$TRAVIS_BUILD_WEB_URL"'",
       "icon_url": "'$AVATAR'"
     },
     "title": "'"$COMMIT_SUBJECT"'",
