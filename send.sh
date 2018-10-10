d909a2f21ac84d7bc74ab948052ef934a0f55b38
Mario Hoyos <mariohoyos92@gmail.com>
2018-10-10 09:36:01 -0500

    chore: update the links across all of repo

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$2" ]; then
-  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/k3rn31p4nic/travis-ci-discord-webhook" && exit
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DiscordHooks/travis-ci-discord-webhook" && exit
 fi
 
 echo -e "[Webhook]: Sending webhook to Discord...\\n";
