44e4042fdf39a3ad301db7873f7f2800af8a97ef
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-11-17 03:23:49 +0000

    set verbose option for curl

--- send.sh
+++ send.sh
@@ -73,5 +73,5 @@ WEBHOOK_DATA='{
   } ]
 }'
 
-(curl --fail --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
+(curl --fail -v --progress-bar -A "TravisCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
